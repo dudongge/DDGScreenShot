8624a520120f82f0dcd194ce20653f3965e5352f
dudongge <dudongge@km.com>
2018-03-28 11:16:11 +0800

    Initial Commit

---
+++ DDGScreenShot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DDGScreenShot
+//
+//  Created by dudongge on 2018/3/28.
+//  Copyright © 2018年 dudongge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
