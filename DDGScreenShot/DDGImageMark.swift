13aa2a517cea3e788b0d3a6c3d47c10b5c5c07d0
dudongge <dudongge@km.com>
2018-03-28 15:44:46 +0800

    Add  imageMark

--- DDGScreenShot/DDGImageMark.swift
+++ DDGScreenShot/DDGImageMark.swift
@@ -14,16 +14,16 @@ class DDGImageMark: UIViewController {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor.white
-//        let image = UIImage(named:"1")?.drawTextInImage(text:"东阁堂主，给你一个star",
-//                                                        textColor: UIColor.blue,
-//                                                        textFont: 40,
-//                                                        textBgColor: UIColor.green,
-//                                                        textX: 20,
-//                                                        textY: 100)
-//        testImageView = UIImageView()
-//        testImageView.image = image
-//        testImageView.frame = CGRect(x: 10, y: 100, width: width - 20 , height: height - 200)
-//        self.view.addSubview(testImageView)
+        let image = UIImage(named:"1")?.drawTextInImage(text:"东阁堂主，给你一个star",
+                                                        textColor: UIColor.blue,
+                                                        textFont: 40,
+                                                        textBgColor: UIColor.green,
+                                                        textX: 20,
+                                                        textY: 100)
+        testImageView = UIImageView()
+        testImageView.image = image
+        testImageView.frame = CGRect(x: 10, y: 100, width: width - 20 , height: height - 200)
+        self.view.addSubview(testImageView)
         
     }
 
